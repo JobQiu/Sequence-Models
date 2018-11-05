e56b9f18002b0707e19666d95a66ac011d7b9689
JobQiu <congminqi@gmail.com>
2018-11-05 15:02:27 -0600

    ...

---
+++ Building a Recurrent Neural Network - Step by Step - Assignment 1/assignment1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 17:57:14 2018
+
+@author: xavier.qiu
+"""
+
+import numpy as np
+from rnn_utils  import *
+
+#%%
+
+def rnn_cell_forward(xt, a_prev, parameters):
+    
+    Wax = parameters["Wax"] 
+    Waa = parameters["Waa"] 
+    Wya = parameters["Wya"] 
+    ba = parameters["ba"] 
+    by = parameters["by"]
+    
+    pass
+
+
+#%%
+
